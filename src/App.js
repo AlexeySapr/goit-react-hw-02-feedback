69c6176859c66c07aeeed2ae57b454cc8bea597c
Alexey Sapr <alexeysapr@gmail.com>
2021-12-12 16:47:53 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Container from './components/container/Container';
+import Header from './components/header/Header';
 
 function App() {
-	return <div className="App">TEST</div>;
+  return (
+    <div className="App">
+      <h1 className="AppHeader">Reviews widget</h1>
+      <Container>
+        <Header />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import s from './Header.module.css';
+
+function Header() {
+  return (
+    <div className={s.header}>
+      <h2>Please leave feedback</h2>
+    </div>
+  );
+}
+
+export default Header;
