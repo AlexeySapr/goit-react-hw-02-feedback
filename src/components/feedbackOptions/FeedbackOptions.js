24e94d305b1ab8f34fdc10e992dd6f3d8b41e91b
Alexey Sapr <alexeysapr@gmail.com>
2021-12-14 09:29:43 +0200

    Add feedbackBtn

--- src/components/feedbackOptions/FeedbackOptions.js
+++ src/components/feedbackOptions/FeedbackOptions.js
@@ -1,18 +1,12 @@
 import PropTypes from 'prop-types';
+import FeedbackBtn from '../feedbackBtn/FeedbackBtn';
 import s from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div className={s.feedback}>
-      {options.map(option => (
-        <button
-          className={s.feedbackBtn}
-          type="button"
-          name={`${option}`}
-          onClick={onLeaveFeedback}
-        >
-          {option}
-        </button>
+      {options.map((option, index) => (
+        <FeedbackBtn key={index} data={option} onBtnClick={onLeaveFeedback} />
       ))}
     </div>
   );
